bda1429946465c7d0a88329e8dc69e57427de82d
EdsonCampanhao <edsonlemos0525@gmail.com>
2023-03-08 10:34:56 -0300

    page ticket

---
+++ src/pages/Ticket/index.js
@@ -0,0 +1,31 @@
+import  Form  from '../../components/Form';
+import Header from '../../components/Header'
+
+import HeroSection from '../../components/HeroSection';
+import ticketBanner from '../../components/HeroSection/img/ticketBanner.jpg'
+
+import Footer from '../../components/Footer'
+
+const Ticket = () => {
+    return (
+        <section>
+
+            <Header
+                itens={['A experiência', 'Mapa de Setores', 'Informações', 'Ingresso']}
+            />
+            <HeroSection 
+            img={ticketBanner}
+            title='Garanta seu Ingresso'
+            />
+            <Form 
+             options={['meia-entrada','inteira','ingresso artista']}
+             />
+             <Footer/>
+
+
+
+        </section>
+
+    )
+}
+export default Ticket;
